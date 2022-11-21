700850cb8f8efff8bcab55b155d18d05b5b828b5
William Henrique Cirino <williamhenriquecirino@hotmail.com>
2022-11-21 12:23:48 -0300

    Manipulando vetor do tipo struck

---
+++ Secao06Ex69/Vetores - PT01/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+double[] vetor = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+//Obtendo os valores do vator
+double soma = 0.0;
+for (int i = 0; i < n; i++)
+{
+    soma += vetor[i];
+}
+
+//Processamento media
+double avg = soma / n;
+
+//Saida da media
+Console.WriteLine($"Media {avg.ToString("F2", CultureInfo.InvariantCulture)}"); //Operaador de interpolacao
