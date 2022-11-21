9f91061685cdf7e43878198d84721190891552c7
William Henrique Cirino <williamhenriquecirino@hotmail.com>
2022-11-21 17:02:51 -0300

    Vetor com tipo instanciação

---
+++ Secao06Ex70/Product.cs
@@ -0,0 +1,8 @@
+namespace Secao06Ex70
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Secao06Ex70/Program.cs
@@ -0,0 +1,32 @@
+﻿using Secao06Ex70;
+using System.Globalization;
+
+Console.Write("Quantidade de Produtos: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Product[] vetor = new Product[n1];
+
+for (int i = 0; i < n1; i++)
+{
+    Console.Write("Nome: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Preco: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    vetor[i] = new Product { Name = name, Price = price };
+}
+
+//somar preço dos produto
+
+double soma = 0.0;
+
+for (int i = 0; i < n1; i++)
+{
+    soma += vetor[i].Price;
+}
+
+Console.WriteLine($"Soma dos Produtos: {soma.ToString("F2")}");
+
+double avg = soma / n1;
+Console.WriteLine($"Media de valor dos Produtos: {avg}");
